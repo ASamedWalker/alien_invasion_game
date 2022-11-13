5730b8dff7618ef1627fa69e678847586a049c3e
Samed Walker <samedwalker50@gmail.com>
2022-11-13 15:43:27 -0500

    Modified initial commit

---
+++ alien_invasion.py
@@ -0,0 +1,50 @@
+import sys
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+  """Overall class to manage game assets and behavior."""
+  def __init__(self) -> None:
+    """Initialize the game, and create game resources."""
+    pygame.init()
+    self.settings = Settings()
+    
+    self.screen = pygame.display.set_mode(
+      (self.settings.screen_width, self.settings.screen_height))
+    
+    pygame.display.set_caption("Alien Invasion")
+    
+    self.ship = Ship(self)
+    
+    # # Setting the background color.
+    # self.bg_color = (230, 230, 230)
+  
+  def run_game(self):
+    """Start the main loop for the game."""
+    while True:
+      """Watch for the keyboard and mouse events."""
+      self._check_events()
+      
+      # Redraw the creen during each pass through the loop.
+      self._update_screen()
+      
+  def _check_events(self):
+    """Respond to keypresses and mouse events."""
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        sys.exit()
+        
+  def _update_screen(self):
+    """Update images on the screen, and flip to the new screen."""
+    self.screen.fill(self.settings.bg_color)
+    self.ship.blitme()
+    
+    pygame.display.flip()
+      
+if __name__ == '__main__':
+  # Make a game instance, and run the game.
+  ai = AlienInvasion()
+  ai.run_game()
