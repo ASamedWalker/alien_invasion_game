0b57643c5ceba31aa1f85cb8349e9423428f4469
Samed Walker <samedwalker50@gmail.com>
2022-11-13 19:42:10 -0500

    Modified initial commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,8 +12,7 @@ class AlienInvasion:
     pygame.init()
     self.settings = Settings()
     
-    self.screen = pygame.display.set_mode(
-      (self.settings.screen_width, self.settings.screen_height))
+    self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
     
     pygame.display.set_caption("Alien Invasion")
     
@@ -28,6 +27,9 @@ class AlienInvasion:
       """Watch for the keyboard and mouse events."""
       self._check_events()
       
+      # Modifying ths ship's update method
+      self.ship.update()
+      
       # Redraw the creen during each pass through the loop.
       self._update_screen()
       
@@ -36,6 +38,28 @@ class AlienInvasion:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         sys.exit()
+      elif event.type == pygame.KEYDOWN:
+        self._check_keydown_events(event)
+          
+      elif event.type == pygame.KEYUP:
+        self.__check_keyup_events(event)
+          
+  def _check_keydown_events(self, event):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+      # Move the ship to the right.
+      self.ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+      self.ship.moving_left = True
+    elif event.key == pygame.K_q:
+      sys.exit()
+      
+  def __check_keyup_events(self, event):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+      self.ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+      self.ship.moving_left = False
         
   def _update_screen(self):
     """Update images on the screen, and flip to the new screen."""
