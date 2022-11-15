031e83d7ad8dcfba6eea5524a1c99692bea12159
Samed Walker <samedwalker50@gmail.com>
2022-11-15 11:29:08 -0500

    Modified initial commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
   """Overall class to manage game assets and behavior."""
@@ -18,8 +19,7 @@ class AlienInvasion:
     
     self.ship = Ship(self)
     
-    # # Setting the background color.
-    # self.bg_color = (230, 230, 230)
+    self.bullets = pygame.sprite.Group()
   
   def run_game(self):
     """Start the main loop for the game."""
@@ -29,6 +29,9 @@ class AlienInvasion:
       
       # Modifying ths ship's update method
       self.ship.update()
+      
+      # Updating the position of the bullets.
+      self._update_bullets()
       
       # Redraw the creen during each pass through the loop.
       self._update_screen()
@@ -53,6 +56,8 @@ class AlienInvasion:
       self.ship.moving_left = True
     elif event.key == pygame.K_q:
       sys.exit()
+    elif event.key == pygame.K_SPACE:
+      self._fire_bullet()
       
   def __check_keyup_events(self, event):
     """Respond to key releases."""
@@ -60,12 +65,30 @@ class AlienInvasion:
       self.ship.moving_right = False
     elif event.key == pygame.K_LEFT:
       self.ship.moving_left = False
+  
+  def _fire_bullet(self):
+    """Create a new bullet and add it to the bullets group."""
+    if len(self.bullets) < self.settings.bullets_allowed:
+      new_bullet = Bullet(self)
+      self.bullets.add(new_bullet)
+      
+  def _update_bullets(self):
+    """Update position of bullets and get rid of old bullets."""
+    self.bullets.update()
+    
+    # Get rid of bullets that have disappeared.
+    for bullet in self.bullets.copy():
+      if bullet.rect.bottom <= 0:
+        self.bullets.remove(bullet)
+      # print(len(self.bullets))
         
   def _update_screen(self):
     """Update images on the screen, and flip to the new screen."""
     self.screen.fill(self.settings.bg_color)
     self.ship.blitme()
-    
+    for bullet in self.bullets.sprites():
+      bullet.draw_bullet()
+      
     pygame.display.flip()
       
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -3,8 +3,18 @@ class Settings:
   
   def __init__(self) -> None:
     """Initialize the game's settings."""
+    
     # Screen settings
     self.screen_width = 1200
     self.screen_height = 800
     self.bg_color = (230, 230, 230)
+    
+    # Speed settings
     self.ship_speed = 1.5
+    
+    # Bullet Settings
+    self.bullet_speed = 1.0
+    self.bullet_width = 3
+    self.bullet_height = 15
+    self.bullet_color = (60, 60, 60)
+    self.bullets_allowed = 3
